54b0dbea4ac268f77bdd50abe61e37e87a602235
CassiaAssis <cassilene.assis@ulife.com.br>
2025-09-18 19:42:18 -0300

    Primeiro commit da minha API

---
+++ src/main/java/com/terceiraAPI/Projeto3aAPI/Projeto3aApiApplication.java
@@ -0,0 +1,13 @@
+package com.terceiraAPI.Projeto3aAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projeto3aApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projeto3aApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/terceiraAPI/Projeto3aAPI/Projeto3aApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.terceiraAPI.Projeto3aAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projeto3aApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
